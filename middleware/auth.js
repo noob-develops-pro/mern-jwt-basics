acb5efbf69a6a69edea55f399e59934563b57e37
usman <kohistani5525@gmail.com>
2023-06-16 23:57:50 +0500

    jwt basics complete

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,28 +1,25 @@
 import jwt from 'jsonwebtoken'
+import CustomAPIError from '../errors/custom-error.js'
 
 const secretKey = process.env.JWT_SECRET
 
 const authMiddleware = (req, res, next) => {
-  // Get the token from the Authorization header
   const token = req.headers.authorization?.split(' ')[1]
-  console.log(token)
 
   // Check if token exists
   if (!token) {
-    return res.status(401).json({ error: 'Unauthorized auth' })
+    throw new CustomAPIError('Unauthorized.. ', 401)
   }
 
   try {
-    // Verify and decode the token
     const decoded = jwt.verify(token, secretKey)
+    console.log(decoded, 'deco')
 
-    // Attach the decoded payload to the request object
     req.user = decoded
 
-    // Move to the next middleware
     next()
   } catch (error) {
-    return res.status(401).json({ error: 'Invalid token' })
+    throw new CustomAPIError('Invalid token', 401)
   }
 }
 
---
+++ reademe.md
@@ -0,0 +1,4 @@
+# JWT BASICS
+
+    - I have learned jwt basics , explored how authorization works.
+    - How to protect resources to be access if not authorized .
